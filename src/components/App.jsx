8291b6e0ab188e5287a2d5e21b002ffb7ab78167
GONZOLOL <glerdie489@g.educaand.es>
2023-02-21 15:25:58 +0100

    localStorage configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,85 @@
-import * as React from "react";
-import Select from 'react-select'
-import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import React from "react";
+import { useState, useEffect } from "react";
 import IconButton from "@mui/material/IconButton";
 import ToggleOffIcon from "@mui/icons-material/ToggleOff";
 import ToggleOnIcon from "@mui/icons-material/ToggleOn";
-import NightlightRoundIcon from "@mui/icons-material/NightlightRound";
 import {ReactComponent as Logo} from "../../starter_files/images/logo.svg";
+import {ReactComponent as ArrowDown} from "../../starter_files/images/icon-arrow-down.svg";
+import {ReactComponent as Moon} from "../../starter_files/images/icon-moon.svg";
 
-
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-
-function Header() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
-
-  const select = [
-    { value: 'sans-serif', label: 'Sans Serif' },
-    { value: 'serif', label: 'Serif' },
-    { value: 'mono', label: 'Mono' }
-  ]
-
-  return (
-    <>
-      <section className="header">
-        <div className="headerWrapper">
-          <Logo className="logo" />
-          <div>
-            <Select options={select} />
-          </div>
-          <IconButton onClick={colorMode.toggleColorMode}>
-            {theme.palette.mode === 'dark' ? <ToggleOnIcon sx={{ color: "hsl(274, 82%, 60%)" }}/> : <ToggleOffIcon color="disabled" />}
-          </IconButton>
-          {theme.palette.mode === 'dark' ? <NightlightRoundIcon sx={{ color: "hsl(274, 82%, 60%)" }}/> : <NightlightRoundIcon color="disabled" />}
-        </div>
-      </section>
-    </>
-  );
-}
 
 export default function App() {
-  const [mode, setMode] = React.useState('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
 
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
+  const [font, setFont] = useState("Sans Serif")
 
+  const [mode, setMode] = useState("light");
+
+  const [menu, setMenu] = useState(false)
+
+  function getLocalStorage() {
+    const localStorageMode = localStorage.getItem('mode');
+
+    if (localStorageMode) {
+      setMode(localStorageMode);
+    }
+  }
+  function updateLocalStorage(mode) {
+    const localStorageMode = localStorage.getItem('mode');
+
+    if (localStorageMode) {
+      localStorage.setItem('mode', localStorageMode);
+      setMode(localStorageMode);
+    }
+  }
+  
+  useEffect(() => {
+    getLocalStorage();
+  }, [])
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  }
+
+  const changeFont = (event) => {
+    setFont(event.target.text);
+    setMenu(!menu);
+  }
+
+  const toggleColorMode = () => {
+      updateLocalStorage(mode === 'light' ? 'dark' : mode === 'dark' ? 'light' : 'light')
+  }
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <Header />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <>
+      <section theme={mode} font={font}>
+        <section className="mainWrapper">
+          <div className="header">
+            <Logo className="logo" />
+            <div className="headerActions">
+              <div id="select">
+                <span>{font}</span>
+              </div>
+              <ArrowDown onClick={toggleMenu} className="arrowDown" />
+              <div className={`menu ${menu ? 'show' : ''}`}>
+                <div className="menuContainer">
+                    <ul>
+                        <li><a href="#" onClick={changeFont}>Sans Serif</a></li>
+                        <li><a href="#" onClick={changeFont}>Serif</a></li>
+                        <li><a href="#" onClick={changeFont}>Mono</a></li>
+                    </ul>
+                </div>
+              </div>
+              <IconButton onClick={toggleColorMode}>
+                {mode === 'dark' ? <ToggleOnIcon sx={{ color: "hsl(274, 82%, 60%)" }}/> : <ToggleOffIcon color="disabled" />}
+              </IconButton>
+              {mode === 'dark' ? <Moon stroke= "hsl(274, 82%, 60%)"/> : <Moon stroke= "hsl(0, 0%, 51%)" />}
+            </div>
+          </div>
+
+          <div className="input">
+
+          </div>
+        </section>
+      </section>
+    </>
   );
 }
